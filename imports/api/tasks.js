29b9297317a420178ac5f3310372cf64a325e32d
slavak <slava.kukuyev@gmail.com>
2017-05-08 00:15:14 +0300

    add basic meteor files

---
+++ imports/api/tasks.js
@@ -0,0 +1,7 @@
+/**
+ * Created by slavak on 5/7/17.
+ */
+
+import { Mongo } from 'meteor/mongo';
+
+export const Tasks = new Mongo.Collection('tasks');
---
+++ imports/ui/body.js
@@ -0,0 +1,33 @@
+/**
+ * Created by slavak on 5/7/17.
+ */
+import {Template} from 'meteor/templating';
+import {Tasks} from '../api/tasks.js';
+import './body.html';
+
+Template.body.helpers({
+    tasks() {
+        return Tasks.find({}, { sort: { createdAt: -1 } });
+        //return Tasks.find({});
+    },
+});
+
+Template.body.events({
+    'submit .new-task'(event) {
+        // Prevent default browser form submit
+        event.preventDefault();
+
+        // Get value from form element
+        const target = event.target;
+        const text = target.text.value;
+
+        // Insert a task into the collection
+        Tasks.insert({
+            text,
+            createdAt: new Date(), // current time
+        });
+
+        // Clear form
+        target.text.value = '';
+    },
+});
